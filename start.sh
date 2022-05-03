ebb9a165309a88e28af758723227f5e6965c37d3
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-05-03 16:02:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Wan-Peng
 fi
-cd /Tigershroff
+cd /Wan-peng
 pip3 install -U -r requirements.txt
 echo "Starting Wan Peng....🔥"
 python3 bot.py
