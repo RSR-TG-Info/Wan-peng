da00c6911af377769f935aca3ba2657851f91d18
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-05-03 17:39:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/Naveen-TG/Wan-peng.git /Wan-peng
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Wan-peng
-fi
+echo "Cloning main Repository"
+git clone -b master https://github.com/Naveen-TG/Wan-peng.git /Wan-peng
 cd /Wan-peng
+echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Wan peng....🔥"
 python3 bot.py
