0bc4b298cf9a5b33d5173c02a990e2cb3948e91b
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-05-03 16:05:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Naveen-TG/Wan-Peng.git /Wan-Peng
+  git clone https://github.com/Naveen-TG/Wan-peng.git /Wan-peng
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Wan-Peng
+  git clone $UPSTREAM_REPO /Wan-peng
 fi
 cd /Wan-peng
 pip3 install -U -r requirements.txt
-echo "Starting Wan Peng....🔥"
+echo "Starting Wan peng....🔥"
 python3 bot.py
